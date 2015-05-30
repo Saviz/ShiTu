88ad6e83ebb37f24b11b0b17fe9d7854c4d757db
buhailiang <buhailiang@gmail.com>
2015-05-30 16:12:51 +0800

    cityname

--- DataHandler/MCDianpingMapResult.m
+++ DataHandler/MCDianpingMapResult.m
@@ -7,6 +7,7 @@
 //
 
 #import "MCDianpingMapResult.h"
+#import "NSString+URLEncoding.h"
 #import "AFNetworking.h"
 
 @implementation MCDianpingMapResult
@@ -19,9 +20,24 @@
 }
 
 - (void)getInfoWithGPS:(CLLocationCoordinate2D)gps {
+    CLGeocoder * geoCoder = [[CLGeocoder alloc] init];
+    CLLocation *loc = [[CLLocation alloc] initWithLatitude:gps.latitude longitude:gps.longitude];
+    [geoCoder reverseGeocodeLocation:loc completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark * placemark in placemarks) {
+            NSString *cityname = [placemark locality];
+            NSLog(@"%@", cityname);
+            NSString *city2 = [[cityname componentsSeparatedByString:@"市"] objectAtIndex:0];
+            [self getInfoWithGPS:gps andCity:[[city2 urlEncodeUsingEncoding:NSUTF8StringEncoding] urlEncodeUsingEncoding:NSUTF8StringEncoding]];
+        }
+    }];
+    
+}
+
+- (void)getInfoWithGPS:(CLLocationCoordinate2D)gps andCity:(NSString *)cityName{
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"application/x-javascript"];
+    
     /*
     
     NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
@@ -46,7 +62,10 @@
     
     //NSString *url = @"http://wap.sogou.com/web/features/vr.jsp?keyword=%E7%BE%8E%E9%A3%9F&qoInfo=query%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26vrQuery%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26classId%3D70008801%26classTag%3DMULTIHIT.LIFE.CATEGORY70008801%26location%3D2%26tplId%3D70008800%26start%3D0%26item_num%3D20%26gpsItemNum%3D150%26pageTurn%3D1%26isGps%3D1%26searchScope%3D500&cb=jsonp5";
     
-    NSString *url = [@"http://wap.sogou.com/tworeq?queryString=%E7%BE%8E%E9%A3%9F&ie=utf8&qoInfo=query%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26vrQuery%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26classId%3D70008801%26classTag%3DMULTIHIT.LIFE.CATEGORY70008801%26location%3D2%26tplId%3D70008800%26start%3D0%26item_num%3D10%26gpsItemNum%3D150%26pageTurn%3D1%26isGps%3D1%26searchScope%3D500%26locationStr%3D%25E5%258C%2597%25E4%25BA%25AC%25E5%25B8%2582%26gps%3D" stringByAppendingFormat:@"%.13f%%257C%.13f", gps.longitude, gps.latitude];
+    //NSString *url = [@"http://wap.sogou.com/tworeq?queryString=%E7%BE%8E%E9%A3%9F&ie=utf8&qoInfo=query%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26vrQuery%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A%25E5%258C%2597%25E4%25BA%25AC%253A%253A0%26classId%3D70008801%26classTag%3DMULTIHIT.LIFE.CATEGORY70008801%26location%3D2%26tplId%3D70008800%26start%3D0%26item_num%3D10%26gpsItemNum%3D150%26pageTurn%3D1%26isGps%3D1%26searchScope%3D500%26locationStr%3D%25E5%258C%2597%25E4%25BA%25AC%25E5%25B8%2582%26gps%3D" stringByAppendingFormat:@"%.13f%%257C%.13f", gps.longitude, gps.latitude];
+    
+    NSString *url = [@"http://wap.sogou.com/tworeq?queryString=%E7%BE%8E%E9%A3%9F&ie=utf8&qoInfo=query%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A--city--%253A%253A0%26vrQuery%3Dclass%253A%253A%25E7%25BE%258E%25E9%25A3%259F%253A%253A0%257C%257Ccity%253A%253A--city--%253A%253A0%26classId%3D70008801%26classTag%3DMULTIHIT.LIFE.CATEGORY70008801%26location%3D2%26tplId%3D70008800%26start%3D0%26item_num%3D10%26gpsItemNum%3D150%26pageTurn%3D1%26isGps%3D1%26searchScope%3D500%26locationStr%3D--city--%25E5%25B8%2582%26gps%3D" stringByAppendingFormat:@"%.13f%%257C%.13f", gps.longitude, gps.latitude];
+    url = [url stringByReplacingOccurrencesOfString:@"--city--" withString:cityName];
     NSLog(@"%@", url);
     [manager
      
@@ -58,12 +77,14 @@
          NSMutableArray *results = [NSMutableArray arrayWithCapacity:10];
          for (int i = 1; i < poixmls.count; i++){
              NSString *xml = [poixmls objectAtIndex:i];
+             NSString *dish = [MCDianpingMapResult extractNode:@"dishname" fromXml:xml];
+             NSString *dishs = [dish stringByReplacingOccurrencesOfString:@";" withString:@","];
              [results addObject:[NSDictionary dictionaryWithObjects:
                 [NSArray arrayWithObjects:
                     [MCDianpingMapResult extractNode:@"img_link" fromXml:xml],
                     [MCDianpingMapResult extractNode:@"key" fromXml:xml],
                     [MCDianpingMapResult extractNode:@"latlng" fromXml:xml],
-                    [MCDianpingMapResult extractNode:@"dishname" fromXml:xml],
+                    dishs,
                     nil
                  ]
                 forKeys:
